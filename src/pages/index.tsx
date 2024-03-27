db08deedc915bb2eb9e6386f5acf6fb707d7b9d1
motojate <motojate@naver.com>
2024-03-27 23:32:13 +0900

    * 네이티브에서 데이터 받아오기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,22 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import AlarmSetter from '@/components/AlarmSetter';
 
 const Home = () => {
   const [alarmTime, setAlarmTime] = useState('');
+
+  useEffect(() => {
+    const handleMessage = (event: any) => {
+      // 메시지 수신 시 로그를 찍어 확인합니다.
+      console.log('Received message: ', event.data);
+    };
+
+    window.addEventListener('message', handleMessage, false);
+
+    // 클린업 함수에서 이벤트 리스너를 제거합니다.
+    return () => {
+      window.removeEventListener('message', handleMessage, false);
+    };
+  }, []);
 
   return (
     <div className="max-w-md mx-auto mt-10 space-y-8">
