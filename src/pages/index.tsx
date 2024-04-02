6b12bf239fee1248e9ce3506337930eed4c32576
motojate <motojate@naver.com>
2024-04-02 18:18:50 +0900

    [BUGFIX] 주석 제거

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,20 +6,18 @@ const Home = () => {
 
   useEffect(() => {
     const handleMessage = (event: any) => {
-      // 메시지 수신 시 로그를 찍어 확인합니다.
       console.log('Received message: ', event.data);
     };
 
     window.addEventListener('message', handleMessage, false);
 
-    // 클린업 함수에서 이벤트 리스너를 제거합니다.
     return () => {
       window.removeEventListener('message', handleMessage, false);
     };
   }, []);
 
   return (
-    <div className="max-w-md mx-auto mt-10 space-y-8">
+    <div className="max-w-md mx-auto mt-10 space-y-8 s">
       <AlarmSetter onSetAlarm={setAlarmTime} />
       <br />
       <br />
